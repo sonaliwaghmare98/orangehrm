213bad334d2325f9f5c806058d13a387c0fe8ae9
sonaliwaghmare9889-debug <sonali.waghmare9889@gmail.com>
2026-08-19 15:59:57 +0530

    Adding allure dependency to pom.xml

--- OrangeHRM/src/test/java/com/skillio/runner/Runner.java
+++ OrangeHRM/src/test/java/com/skillio/runner/Runner.java
@@ -2,7 +2,18 @@ package com.skillio.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/resources/Features",glue = "com.skillio.stepDefinitions",tags = "@Smoke")
+
+@CucumberOptions(features = "src/test/resources/Features",
+        glue = "com.skillio",
+        tags = "not @Smoke", dryRun=false)
 public class Runner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
+
